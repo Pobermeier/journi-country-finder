a1447571ec14f8a8829b94a410015ccc9310fb72
Patrick Obermeier <patrick.obermeier@outlook.com>
2022-11-24 22:11:30 +0100

    add comments

--- src/utils/getDistance/getDistance.test.ts
+++ src/utils/getDistance/getDistance.test.ts
@@ -1,10 +1,13 @@
 import getDistance from "./getDistance";
 
 describe("getDistance", () => {
+  // Vienna
   const testLat1 = 48.210033;
   const testLng1 = 16.363449;
-  const testLat2 = 60.30516811641584;
-  const testLng2 = 15.439302452417918;
+  // Italy
+  const testLat2 = 41.87194;
+  const testLng2 = 12.5674;
+  // Australia
   const testLat3 = -31.21759246133962;
   const testLng3 = 151.1243932822211;
 
@@ -15,7 +18,9 @@ describe("getDistance", () => {
   });
 
   it("returns correct distance if the latitude/longitude of two locations is different", () => {
-    const expectedDistance1 = 1346.2226737000303;
+    // Expected distance between Vienna and Italy
+    const expectedDistance1 = 765.0288354944954;
+    // Expected distance between Vienna and Australia
     const expectedDistance2 = 15786.832538988703;
 
     expect(getDistance(testLat1, testLng1, testLat2, testLng2)).toBe(expectedDistance1);
