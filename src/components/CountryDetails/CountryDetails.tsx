e32819baa909d3e62b953838c18c1053946cd78d
Patrick Obermeier <patrick.obermeier@outlook.com>
2022-11-26 17:27:08 +0100

    wrap component in React.memo

--- src/components/CountryDetails/CountryDetails.tsx
+++ src/components/CountryDetails/CountryDetails.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import Image from "next/image";
 import { type CountryClient } from "models/country";
 import CountryStat from "components/CountryStat";
@@ -50,4 +51,4 @@ const CountryDetails = ({ country }: CountryDetailsProps) => {
   );
 };
 
-export default CountryDetails;
+export default memo(CountryDetails);
