2dfc9e3b490542de393f20e0eed09533c60d3340
Patrick Obermeier <patrick.obermeier@outlook.com>
2022-11-27 09:56:20 +0100

    remove react-query-devtools from project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import MainLayout from "layouts/MainLayout";
 import "styles/globals.css";
 
@@ -9,8 +8,6 @@ const queryClient = new QueryClient();
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* The react-query devtools are automatically disabled in production */}
-      <ReactQueryDevtools initialIsOpen={false} />
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
