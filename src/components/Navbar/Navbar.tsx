056966da886453865fe75e58680dae07f805d82f
Patrick Obermeier <patrick.obermeier@outlook.com>
2022-11-27 13:01:03 +0100

    reduced markup inside jsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,14 +4,10 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div className="flex h-16 justify-center md:justify-between">
-        <div className="flex">
-          <div className="flex flex-shrink-0 items-center">
-            <Link href="/">
-              <Image className="h-8 w-auto" src="/logo.svg" alt="Journi" width={146} height={32} />
-            </Link>
-          </div>
-        </div>
+      <div className="flex py-4 justify-center md:justify-between">
+        <Link href="/">
+          <Image className="h-8 w-auto" src="/logo.svg" alt="Journi" width={146} height={32} />
+        </Link>
       </div>
     </div>
   );
