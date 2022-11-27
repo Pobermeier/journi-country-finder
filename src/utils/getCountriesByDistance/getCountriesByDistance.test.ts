e226ba71e7229fdd2815f1002b51bf948d2343de
Patrick Obermeier <patrick.obermeier@outlook.com>
2022-11-27 14:19:59 +0100

    fix type errors

--- src/utils/getCountriesByDistance/getCountriesByDistance.test.ts
+++ src/utils/getCountriesByDistance/getCountriesByDistance.test.ts
@@ -1,6 +1,9 @@
+import { type Country } from "models/country";
 import getCountriesByDistanceToUser from "./getCountriesByDistance";
 import countries from "db/countries-metadata.json";
 import expectedCountries from "./getCountriesByDistance.mock.json";
+
+const testCountries = countries as Country[];
 
 describe("getCountriesByDistance", () => {
   it("returns correctly sorted countries", () => {
@@ -8,7 +11,7 @@ describe("getCountriesByDistance", () => {
     const testLat1 = 48.210033;
     const testLng1 = 16.363449;
 
-    expect(getCountriesByDistanceToUser(countries, testLat1, testLng1)).toStrictEqual(
+    expect(getCountriesByDistanceToUser(testCountries, testLat1, testLng1)).toStrictEqual(
       expectedCountries,
     );
   });
--- src/utils/getCountriesByTerm/getCountriesByTerm.test.ts
+++ src/utils/getCountriesByTerm/getCountriesByTerm.test.ts
@@ -1,11 +1,14 @@
+import { type Country } from "models/country";
 import getCountriesByTerm from "./getCountriesByTerm";
 import countries from "db/countries-metadata.json";
 import expectedCountries from "./getCountriesByTerm.mock.json";
+
+const testCountries = countries as Country[];
 
 describe("getCountriesByTerm", () => {
   it("returns correctly filtered countries", () => {
     const testTerm = "aust";
 
-    expect(getCountriesByTerm(countries, testTerm)).toStrictEqual(expectedCountries);
+    expect(getCountriesByTerm(testCountries, testTerm)).toStrictEqual(expectedCountries);
   });
 });
